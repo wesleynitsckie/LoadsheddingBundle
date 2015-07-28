053cd14e82b836e3776339a7e3f159925768f0db
wesleynitsckie <wesleynitsckie@gmail.com>
2015-07-28 14:29:50 +0200

    First round commit

---
+++ BredieBox/LoadSheddingBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BredieBox\LoadSheddingBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('LoadSheddingBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ BredieBox/LoadSheddingBundle/LoadSheddingBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BredieBox\LoadSheddingBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LoadSheddingBundle extends Bundle
+{
+}
